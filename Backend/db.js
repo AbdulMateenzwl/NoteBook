a38f7118ac1469486f82cedacf8011568eb8431a
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-07-25 21:43:59 +0500

    express validator added

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directconnection=true&ssl=false';
+const mongoURI = 'mongodb://localhost:27017/notebook?readPreference=primary&appname=MongoDB%20Compass&directconnection=true&ssl=false';
 
 const connectToMongo = () => {
 
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,15 +1,32 @@
 const express = require('express');
-const User = require('../models/User')
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
+router.post(
+    '/',
+    [
+        body('name', 'Enter a valid Name').isLength({ min: 3 }),
+        body('email', 'Email not Valid').isEmail(),
+        body('password', 'Password must be atleast 5 characters').isLength({
+            min: 5,
+        }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err)
+            res.json({error: 'Email is already taken. Sign in instead.',message: err.message})
+        })
+    }
+);
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    // res.json([])
-    res.send(req.body)
-})
-
-
-module.exports = router
+module.exports = router;
