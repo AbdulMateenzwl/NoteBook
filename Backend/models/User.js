54f854fd6b807a1136ce137ec25d5af044d5ece9
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-07-26 15:01:47 +0500

    create User auth, email duplicacy error

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const UserSchema = new Schema({
     name: {
         type: String,
@@ -12,14 +12,13 @@ const UserSchema = new Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
     date: {
         type: Date,
-        default: Date.now
+        default: Date.now,
     },
 });
 
-const User = mongoose.model("user", UserSchema);
-User.createIndexes();
+const User = mongoose.model('user', UserSchema);
 module.exports = User;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,8 +3,9 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
+// Create a User using :Post "/api/auth/". Doesn't require Auth
 router.post(
-    '/',
+    '/createuser',
     [
         body('name', 'Enter a valid Name').isLength({ min: 3 }),
         body('email', 'Email not Valid').isEmail(),
@@ -12,20 +13,33 @@ router.post(
             min: 5,
         }),
     ],
-    (req, res) => {
+    async (req, res) => {
+        // If there are errors, return Bad request and the errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         }
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        }).then((user) => res.json(user))
-        .catch((err) => {
-            console.log(err)
-            res.json({error: 'Email is already taken. Sign in instead.',message: err.message})
-        })
+
+        // Check whether user with email User.exists
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res
+                    .status(400)
+                    .json({
+                        error: 'Sorry a user with this email already exists.',
+                    });
+            }
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            });
+            res.json(user);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Some Error Occured');
+        }
     }
 );
 
