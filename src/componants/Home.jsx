7122662128439f8d97569dbfd5ba0d1619bced5a
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-07-31 14:35:17 +0500

    displaying notes in react

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import Navbar from './NavBar'
+import React, { useContext } from 'react'
+import noteContext from '../context/Notes/noteContext';
 
 export default function Home() {
+  const context = useContext(noteContext);
+  const { notes, setNotes } = context;
+
   return (
     <div className='container'>
       <h2>Add a Note</h2>
       <form>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1"/>
+          <input type="password" className="form-control" id="exampleInputPassword1" />
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
       <h2>Your Note</h2>
+      {
+        notes.map((note) => {
+          return (
+            <h1>{note.title}</h1>
+          )
+        })
+      }
     </div>
   )
 }
--- src/context/Notes/NoteState.jsx
+++ src/context/Notes/NoteState.jsx
@@ -2,9 +2,43 @@ import react from 'react';
 import NoteContext from './noteContext';
 import { useState } from 'react';
 const NoteState = (props) => {
-    
+
+    const notesInitial = [
+        {
+            "_id": "64c2baedde9ce9cca1e8f288",
+            "user": "64c1061384e44fcbedb38e07",
+            "title": "my title",
+            "description": "please do something",
+            "tag": "personal",
+            "date": "2023-07-27T18:43:57.318Z",
+            "__v": 0
+        },
+        {
+            "_id": "64c77dfaaaedfa6e6be9d11c",
+            "user": "64c1061384e44fcbedb38e07",
+            "title": "my title",
+            "description": "please do something",
+            "tag": "personal",
+            "date": "2023-07-31T09:25:14.899Z",
+            "__v": 0
+        },
+        {
+            "_id": "64c77fb7aaedfa6e6be9d11f",
+            "user": "64c1061384e44fcbedb38e07",
+            "title": "note 1",
+            "description": "note 1 is about testing",
+            "tag": "personal",
+            "date": "2023-07-31T09:32:39.175Z",
+            "__v": 0
+        }
+    ];
+
+    const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
+        <NoteContext.Provider value={{notes,setNotes}}>
+            {props.children}
+        </NoteContext.Provider>
     );
 };
 
