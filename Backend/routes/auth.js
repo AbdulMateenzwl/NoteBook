85ceeef41539e7c0e0a101b4c6d6156840a318a3
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-08-04 00:46:41 +0500

    signup page

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -18,10 +18,11 @@ router.post(
 		}),
 	],
 	async (req, res) => {
+		let success = false;
 		// If there are errors, return Bad request and the errors
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-			return res.status(422).json({ errors: errors.array() });
+			return res.status(422).json({  errors: errors.array() });
 		}
 
 		// Check whether user with email User.exists
@@ -45,9 +46,9 @@ router.post(
 			let data = {
 				user: user.id,
 			};
-
+			success = true;
 			const authToken = jwt.sign(data, JWT_SECRET);
-			res.json(authToken);
+			res.json({success,authToken});
 		} catch (error) {
 			console.error(error.message);
 			res.status(500).send('Some (Server)Error Occured');
